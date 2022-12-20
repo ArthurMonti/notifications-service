35108a2fdba87c252f9c47087a4e959e05b086e2
Arthur Monti <arthur_monti@hotmail.com>
2022-12-20 15:47:07 -0300

    aula 2

---
+++ test/repositories/in-memory-notifications-respository.ts
@@ -0,0 +1,10 @@
+import { Notification } from "@application/entities/notification";
+import { NotificationsRepository } from "@application/repositories/notification-repository";
+
+export class InMemoryNotificationsRepository implements NotificationsRepository {
+  public notifications: Notification[] = [];
+
+  async create(notification: Notification): Promise<void> {
+    this.notifications.push(notification);
+  }
+}
