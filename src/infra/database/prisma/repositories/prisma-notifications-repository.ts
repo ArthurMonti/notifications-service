b6ca80387564297b4306a482a74b97515b7df3c7
Arthur Monti <arthur_monti@hotmail.com>
2022-12-20 23:55:01 -0300

    Aula 3 - Finalizado

--- src/infra/database/prisma/repositories/prisma-notifications-repository.ts
+++ src/infra/database/prisma/repositories/prisma-notifications-repository.ts
@@ -2,23 +2,60 @@ import { Injectable } from "@nestjs/common";
 import { Notification } from "src/application/entities/notification";
 import { NotificationsRepository } from "@application/repositories/notification-repository";
 import { PrismaService } from "../prisma.service";
+import { PrismaNotificationMapper } from "../mappers/prisma-notification-mapper";
 
 @Injectable()
 export class PrismaNotificationsRepository implements NotificationsRepository{
-  constructor(private prismaService: PrismaService){}
-  
-  
+  constructor(private prisma: PrismaService){}
+
   async create(notification: Notification): Promise<void> {
-    await this.prismaService.notification.create({
-      data: {
-        id: notification.id,
-        content: notification.content.value,
-        category: notification.category,
-        recipientId: notification.recipientId,
-        readAt: notification.readAt,
-        caretedAt: notification.createdAt
-      }
-    })
+    const raw = PrismaNotificationMapper.toPrisma(notification)
+    
+    await this.prisma.notification.create({
+      data: raw,
+    });
+  }
+
+  async save(notification: Notification): Promise<void> {
+    const raw = PrismaNotificationMapper.toPrisma(notification);
+
+    await this.prisma.notification.update({
+      where: {
+        id: raw.id,
+      },
+      data: raw
+    });
+  }
+
+  async findById(notificationId: string): Promise<Notification | null> {
+    const notification = await this.prisma.notification.findUnique({
+      where: {
+        id: notificationId,
+      },
+    });
+
+    if(!notification)
+      return null;
+
+    return PrismaNotificationMapper.toDomain(notification);
+  }
+
+  async countManyByRecipientId(recipientId: string): Promise<number> {
+    return await this.prisma.notification.count({
+      where: {
+        recipientId,
+      },
+    });
+
+  }
+  async findManyByRecipientId(recipientId: string): Promise<Notification[]> {
+      const notifications = await this.prisma.notification.findMany({
+        where: {
+          recipientId
+        },
+      })
+
+      return notifications.map(PrismaNotificationMapper.toDomain);
   }
 
 }
--- src/infra/http/controller/notifications.controller.ts
+++ src/infra/http/controller/notifications.controller.ts
@@ -1,12 +1,24 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { CancelNotification } from '@application/use-cases/cancel-notification';
+import { CountRecipientNotifications } from '@application/use-cases/count-recipient-notification';
+import { GetRecipientNotifications } from '@application/use-cases/get-recipient-notifications';
+import { ReadNotification } from '@application/use-cases/read-notification';
+import { UnreadNotification } from '@application/use-cases/unread-notification';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { SendNotification } from 'src/application/use-cases/send-notification';
 import { CreateNotificationBody } from '../dtos/create-notification-body';
+import { NotificationViewModel } from '../view-models/notification-view-model';
 
 @Controller('notifications')
 export class NotificationsController {
   constructor(
-    private sendNotification : SendNotification
-  ){}
+    private sendNotification: SendNotification,
+    private cancelNotification: CancelNotification,
+    private readNotification: ReadNotification,
+    private unreadNotification: UnreadNotification,
+    private countRecipientNotifications: CountRecipientNotifications,
+    private getRecipientNotificationss: GetRecipientNotifications
+    ) {}
+    
   @Post()
   async create(@Body() body: CreateNotificationBody) {
     console.log(body)
@@ -17,6 +29,55 @@ export class NotificationsController {
       category
     });
 
-    return {notification};
+    return {notification: NotificationViewModel.toHTTP(notification)};
   }
+
+
+  @Get('from/:recipientId')
+  async getFromRecipient(@Param('recipientId') recipientId: string) {
+    const { notifications }= await this.getRecipientNotificationss.execute({
+      recipientId
+    })
+    return {
+      notifications: notifications.map(NotificationViewModel.toHTTP) 
+    };
+  }
+
+  @Get('count/from/:recipientId')
+  async countFromRecipient(@Param('recipientId') recipientId: string): Promise<{count: number}> {
+    const { count }= await this.countRecipientNotifications.execute({
+      recipientId
+    })
+    return {
+      count
+    };
+  }
+
+  @Patch(':id/read')
+  async read(@Param('id') id: string) {
+    await this.readNotification.execute({
+      notificationId: id
+    });
+  }
+  
+  @Patch(':id/unread')
+  async unread(@Param('id') id: string){
+    await this.unreadNotification.execute({
+      notificationId: id
+    });
+  }
+
+  @Patch(':id/cancel')
+  async cancel(@Param('id') id: string) {
+    await this.cancelNotification.execute({
+      notificationId: id,
+    });
+  }
+
+
+
+
+
+
+
 }
